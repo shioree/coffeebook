53e4ff1f2e3cea5fe2679249a04dae4bd19e3e6e
shioree <yuta.iida.sh@icloud.com>
2020-06-06 18:21:14 +0900

    トップページの仮レイアウト実装

--- ClientApp/coffeebook/src/app/app-routing.module.ts
+++ ClientApp/coffeebook/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MainComponent } from './main/main.component';
+import { RecordComponent } from './record/record.component';
+import { BrowseComponent } from './browse/browse.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'record/drip', component: RecordComponent },
+  { path: 'browse/by-conditions', component: BrowseComponent },
+  { path: '**', component: MainComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ClientApp/coffeebook/src/app/app.module.ts
+++ ClientApp/coffeebook/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecordComponent } from './record/record.component';
+import { BrowseComponent } from './browse/browse.component';
+import { MainComponent } from './main/main.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { TipsComponent } from './tips/tips.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordComponent,
+    BrowseComponent,
+    MainComponent,
+    ToolbarComponent,
+    TipsComponent
   ],
   imports: [
     BrowserModule,
