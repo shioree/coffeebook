c372c83ea64aabf300b1eb08f84766e06ce955a1
shioree <yuta.iida.sh@icloud.com>
2020-06-12 22:22:46 +0900

    画像ファイルの移動、定数ファイルの作成

---
+++ ClientApp/coffeebook/src/app/api/const.ts
@@ -0,0 +1,5 @@
+
+export const Const = {
+  URL_CONFIG: '../../assets/config/config.json',
+  URL_BACKEND: 'https://coffeebook.azurewebsites.net/api/RegisterRecipe?code=ZAlHVadVsDmQxxrobs1L/XaTpa8e595lNM4nEGJOP/3sHARf1IsAUw=='
+};
