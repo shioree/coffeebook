fc9174f6ce076db03b92708c6b6a8041a79607a8
shioree <yuta.iida.sh@icloud.com>
2020-06-14 19:55:12 +0900

    レシピ閲覧処理のバックエンドを仮実装した

---
+++ coffeebook/coffeebook/FetchRecipes.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace coffeebook
+{
+    public static class FetchRecipes
+    {
+        [FunctionName("FetchRecipes")]
+        public static async Task<IEnumerable<Recipe>> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log,
+            ExecutionContext context)
+        {
+            log.LogInformation("レシピ取得処理を実行します");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = config.GetConnectionString("cosmosdb-connection-string");
+            var client = new CosmosClient(connectionString);
+            var container = client.GetContainer("coffeebook-db", "Recipes");
+
+            var query = container.GetItemQueryIterator<Recipe>(new QueryDefinition(
+                "select * from r where r.userId = @userId")
+                .WithParameter("@userId", "テストユーザ"));
+            List<Recipe> results = new List<Recipe>();
+            while (query.HasMoreResults)
+            {                
+                var response = await query.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
+
+            return results;
+        }
+    }
+}
---
+++ coffeebook/coffeebook/Recipe.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace coffeebook
+{
+    public partial class Recipe
+    {
+        [JsonProperty("beans")]
+        public Beans Beans { get; set; }
+
+        [JsonProperty("style")]
+        public Style Style { get; set; }
+
+        [JsonProperty("evaluation")]
+        public Evaluation Evaluation { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("memo")]
+        public string Memo { get; set; }
+
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+    }
+
+    public partial class Beans
+    {
+        [JsonProperty("origin")]
+        public string Origin { get; set; }
+
+        [JsonProperty("shop")]
+        public string Shop { get; set; }
+
+        [JsonProperty("roast")]
+        public string Roast { get; set; }
+
+        [JsonProperty("grain")]
+        public string Grain { get; set; }
+    }
+
+    public partial class Evaluation
+    {
+        [JsonProperty("rating")]
+        public string Rating { get; set; }
+
+        [JsonProperty("acidity")]
+        public string Acidity { get; set; }
+
+        [JsonProperty("bitterness")]
+        public string Bitternesst { get; set; }
+
+        [JsonProperty("density")]
+        public string Density { get; set; }
+
+        [JsonProperty("otherFeatures")]
+        public string OtherFeatures { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+    }
+
+    public partial class Style
+    {
+        [JsonProperty("beansAmount")]
+        public string BeansAmount { get; set; }
+
+        [JsonProperty("steamTime")]
+        public string SteamTime { get; set; }
+
+        [JsonProperty("extractionTime")]
+        public string ExtractionTime { get; set; }
+
+        [JsonProperty("productAmount")]
+        public string ProductAmount { get; set; }
+    }
+}
